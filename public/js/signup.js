1e007d4fd0c1fa522ebd50aab6f6c74961245285
Roee Eidan <roeeeidan@gmail.com>
2018-05-05 23:45:10 -0400

    Adding email validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,25 @@
+
+
+$('input')
+    .focus(function () {
+        $(this).attr('placeholder', ' ')
+    })
+    .blur(function () {
+        $(this).attr('placeholder', 'Your email')
+    })
+
+function validateEmail() {
+    email = $('input').val()
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase())
+}
+
+
 $('.button').on('click', function (e) {
-    e.preventDefault();
+    e.preventDefault()
+
+    if (!validateEmail())
+        return $('#err').show()
 
     $.ajax({
         url: 'https://script.google.com/macros/s/AKfycbw2N0X2-GkZp_9k6FP5JPYQ1c3no8ItSXp9ipWv4UmA2CMoHeg/exec',
